06f16edc4b0cab3094a34a46c1cdc2c03dcfd4cd
Phillip Vo <heybean41@gmail.com>
2022-07-07 16:36:06 -0700

    Adds most leaders into scoring script

---
+++ scienceSelector.lua
@@ -0,0 +1,21 @@
+SelectedScience = ""
+
+function onObjectEnterZone(zone, object)
+    if object == self then
+        self.UI.setAttribute("scienceSelect", "active", false)
+    end
+end
+
+function onObjectDrop(player, object)
+    if object == self and #object.getZones() <= 0 then
+        self.UI.setAttribute("scienceSelect", "active", true)
+    end
+end
+
+function symbolClicked(player, value, id)
+    if value == "True" then
+        SelectedScience = id
+    else
+        SelectedScience = ""
+    end
+end
